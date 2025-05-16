d4210f9da499ff574f5a9a5fd2a5b582740257aa
rajan <rajanvasani9@gmail.com>
2025-05-16 17:10:38 +0530

    solved bug of router

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const router = express.Router
+const router = express.Router()
 const userSchema = require('./models/authSchema')
 
 const register = async (req, res) => {
